15b0947f7d3b55a4adfad0bc90af2b08442fd4a3
dustin10027 <sanchezdustin100@gmail.com>
2022-06-11 21:51:06 -0500

    Initial commit

---
+++ app/src/main/java/com/readsystem/ecommerceapp/activities/PaymentActivity.java
@@ -0,0 +1,64 @@
+package com.readsystem.ecommerceapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.readsystem.ecommerceapp.R;
+
+import org.json.JSONObject;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    double amount= 0.0;
+    Toolbar toolbar;
+    TextView subTotal,descuento,envio,total;
+    Button paymentBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        //Toolbar
+        toolbar = findViewById(R.id.payment_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        amount= getIntent().getDoubleExtra("Monto",0.0);
+
+        subTotal= findViewById(R.id.sub_total);
+        descuento= findViewById(R.id.textView17);
+        envio= findViewById(R.id.textView18);
+        total= findViewById(R.id.total_amt);
+        paymentBtn= findViewById(R.id.pay_btn);
+
+        subTotal.setText("S/. "+amount);
+
+        paymentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+
+    }
---
+++ app/src/main/java/com/readsystem/ecommerceapp/fragments/SlideModel.java
@@ -0,0 +1,6 @@
+package com.readsystem.ecommerceapp.fragments;
+
+public class SlideModel {
+    public SlideModel(int banner1, String descuento_en_artículos_de_zapatos, Object centerCrop) {
+    }
+}
